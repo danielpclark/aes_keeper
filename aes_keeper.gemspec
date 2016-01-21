92252c68daec13acb71969476e8b1e558be2afd7
Daniel P. Clark <6ftdan@gmail.com>
2016-01-21 16:51:17 -0500

    Minitest suite

--- aes_keeper.gemspec
+++ aes_keeper.gemspec
@@ -21,5 +21,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "minitest", "~> 5.8"
+  spec.add_development_dependency "minitest-reporters", "~> 1.1.7"
+  spec.add_development_dependency "color_pound_spec_reporter", "~> 0.0.5"
   spec.add_runtime_dependency "armor", "~> 0.0"
 end
---
+++ test/aes_keeper_test.rb
@@ -0,0 +1,48 @@
+require 'minitest_helper'
+
+describe AesKeeper do
+  let(:uuid)    { SecureRandom.uuid }
+  let(:encrypt1){AesKeeper.new key: "asdfghjklqwertyuiopzxcvbnm1234567890"}
+  let(:encrypt2){AesKeeper.new key: "asdfghjklqwertyuiopzxcvbnm1234567890", salt: "shaker"}
+  let(:encrypt3){AesKeeper.new key: uuid, salt: "Example.com"}
+
+  it "produces a hash of size 2" do
+    hsh = encrypt1.encrypt("moo")
+    _(hsh).must_be_kind_of Hash
+    _(hsh.size).must_be_same_as 2
+    _(hsh).must_include :iv
+    _(hsh).must_include :encrypted
+  end
+
+  it "can be as a string as well" do
+    str = encrypt2.encrypt("moo").to_s
+    _(str).must_be_kind_of String
+    _(str.count(":")).must_be_same_as 1
+  end
+
+  it "encrypts and decrypts #1" do
+    a = encrypt1.encrypt("moo")
+    _(encrypt1.decrypt(a)).must_equal "moo"
+  end
+
+  it "encrypts and decrypts #2" do
+    b = encrypt2.encrypt("moo")
+    _(encrypt2.decrypt(b)).must_equal "moo"
+  end
+
+  it "encrypts and decrypts #3" do
+    c = encrypt1.encrypt("moo").to_s
+    _(encrypt1.decrypt(c)).must_equal "moo"
+  end
+
+  it "encrypts and decrypts #4" do
+    d = encrypt2.encrypt("moo").to_s
+    _(encrypt2.decrypt(d)).must_equal "moo"
+  end
+
+  it "encrypts and decrypts #5" do
+    e = encrypt3.encrypt(uuid).to_s
+    _(encrypt3.decrypt(e)).must_equal uuid    
+  end
+end
+
